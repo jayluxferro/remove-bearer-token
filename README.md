85810f82543050a1874c9b3b375706908401a586
Jay <jayluxferro@sperixlabs.org>
2022-12-08 21:53:52 +0000

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+## remove-bearer-token
+
+This BurpSuite extension remove HTTP request bearer tokens when performing security audits.
+
+Download the latest extension from https://github.com/jayluxferro/remove-bearer-token/releases
---
+++ src/Util.kt
@@ -0,0 +1,10 @@
+package burp
+
+
+class Util {
+    companion object {
+        const val appName = "Remove Bearer Token"
+        const val appVersion = "1.0.0"
+        val headers = arrayListOf("Authorization", "Bearer")
+    }
+}
